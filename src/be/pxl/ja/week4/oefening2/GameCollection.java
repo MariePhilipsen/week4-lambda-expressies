110a64df71cd5b374701424eeaa4a01adb498d9d
Marie Philipsen <marie_philipsen@hotmail.com>
2018-10-17 09:19:02 +0200

    toevoegen methode selectGames met gebruik van Predicate interface

--- src/be/pxl/ja/week4/oefening2/GameCollection.java
+++ src/be/pxl/ja/week4/oefening2/GameCollection.java
@@ -1,6 +1,9 @@
 package be.pxl.ja.week4.oefening2;
 
+import com.sun.org.apache.xerces.internal.dom.PSVIDOMImplementationImpl;
+
 import java.util.ArrayList;
+import java.util.function.Predicate;
 
 public class GameCollection {
 
@@ -8,5 +11,17 @@ public class GameCollection {
 
     public void addGame(VideoGame videoGame){
         list.add(videoGame);
+    }
+
+    public ArrayList<VideoGame> selectGames(Predicate<VideoGame> videoGameTest){
+        ArrayList<VideoGame> result = new ArrayList<>();
+
+        for (VideoGame aList : list) {
+            if (videoGameTest.test(aList)) {
+                result.add(aList);
+            }
+
+        }
+        return result;
     }
 }
