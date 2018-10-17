c0353c5d59b7e863e0ec38a0ecf3e5aac067fe36
Marie Philipsen <marie_philipsen@hotmail.com>
2018-10-17 09:35:47 +0200

    in GameBrowser klasse een methode showGamesForSearch eerst met behulp van anonieme blok -> vervanging naar Lambda

--- src/be/pxl/ja/week4/oefening2/GameBrowser.java
+++ src/be/pxl/ja/week4/oefening2/GameBrowser.java
@@ -1,5 +1,8 @@
 package be.pxl.ja.week4.oefening2;
 
+import java.util.ArrayList;
+import java.util.function.Predicate;
+
 public class GameBrowser {
 
     private GameCollection gameCollection;
@@ -8,4 +11,35 @@ public class GameBrowser {
     public GameBrowser(GameCollection gameCollection) {
         this.gameCollection = gameCollection;
     }
+
+/*    public ArrayList<VideoGame> showGamesForSearch(String search){
+        return gameCollection.selectGames(new Predicate<VideoGame>() {
+            @Override
+            public boolean test(VideoGame videoGame) {
+                if(videoGame.getName().toLowerCase().contains(search.toLowerCase())){
+                    return true;
+                }
+                return false;
+            }
+        });
+
+    }*/
+
+    public ArrayList<VideoGame> showGamesForSearch(String search){
+        return gameCollection.selectGames(videoGame -> {
+            if(videoGame.getName().toLowerCase().contains(search.toLowerCase())){
+                return true;
+            }
+            return false;
+        });
+
+    }
+
+    // kan nog korter -> if -> lampje, simplify
 }
+
+
+
+
+
+
